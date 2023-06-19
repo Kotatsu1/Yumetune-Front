46b7586405b68eac88a362802154d08c4bc17485
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-06-19 16:08:55 +0700

    feat: update Song

--- src/entities/Song/ui/Song/Song.tsx
+++ src/entities/Song/ui/Song/Song.tsx
@@ -1,11 +1,11 @@
-import { ISong } from "@/entities/Song/types";
+import { Song } from "@/entities/Song/types";
 
-const Song = ({ img, alt, title, artist, album, duration }: ISong) => {
+const Song = ({ img, alt, title, artist, album, duration }: Song) => {
   return (
     <div className="flex items-center justify-between gap-4">
       <div className="flex items-center gap-4">
         <img src={img} alt={alt} />
-        {title}
+        <p>{title}</p>
       </div>
       <p>{artist}</p>
       <p>{album}</p>

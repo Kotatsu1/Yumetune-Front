7831999df012651617d3b700aef29b3334680f9e
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-06-22 15:57:41 +0700

    feat: update Song

--- src/entities/Song/ui/Song/Song.tsx
+++ src/entities/Song/ui/Song/Song.tsx
@@ -4,7 +4,8 @@ const Song = ({ img, alt, title, artist, album, duration }: Song) => {
   return (
     <div className="flex items-center justify-between gap-4">
       <div className="flex items-center gap-4">
-        <img src={img} alt={alt} />
+        <button>play</button>
+        <img className="rounded" src={img} alt={alt} />
         <p>{title}</p>
       </div>
       <p>{artist}</p>

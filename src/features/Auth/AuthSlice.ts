6eb4c52d0f531e42c60ab31fa72188d038b7a95c
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-06-22 15:57:56 +0700

    feat: update AuthSlice

--- src/features/Auth/AuthSlice.ts
+++ src/features/Auth/AuthSlice.ts
@@ -2,21 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface AuthState {
   isAuthenticated: boolean;
+  token: string | null;
 }
 
 const initialState: AuthState = {
   isAuthenticated: false,
+  token: null,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setAuthenticated(state) {
+    setAuthenticated(state, action) {
       state.isAuthenticated = true;
+      state.token = action.payload;
+      localStorage.setItem("token", action.payload);
     },
     clearAuthenticated(state) {
       state.isAuthenticated = false;
+      state.token = null;
+      localStorage.removeItem("token");
     },
   },
 });

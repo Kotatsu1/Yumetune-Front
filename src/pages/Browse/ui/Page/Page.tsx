98783dde50e1040fb6dd80ea0f4952868416bd15
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-06-29 05:21:19 +0700

    feat: add Browse

---
+++ src/pages/Browse/ui/Page/Page.tsx
@@ -0,0 +1,43 @@
+const Browse = () => {
+  return (
+    <>
+      <section>
+        <div className="container py-5 flex flex-col gap-y-10">
+          <div className="flex justify-between gap-x-5">
+            <div className="w-1/3 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+            <div className="w-1/3 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+            <div className="w-1/3 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+          </div>
+          <div className="flex justify-between gap-x-5">
+            <div className="w-1/6 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+            <div className="w-1/6 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+            <div className="w-1/6 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+            <div className="w-1/6 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+            <div className="w-1/6 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+            <div className="w-1/6 flex justify-center items-center bg-base-200 rounded h-48">
+              <h2 className="text-white font-bold text-3xl">Музыка № 1</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Browse;
